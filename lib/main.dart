fffcf190116462cbf450fe0d67ab2f86a95524a2
MennaTaha17 <elshreeftaha79@gmail.com>
2025-11-06 10:13:03 +0200

    forget password ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:movies/common/app_colors.dart';
+import 'package:movies/otp_verfication.dart';
+import 'package:movies/screens/forget_password_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,9 +16,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Movies App',
-      home: Scaffold(
-        body:Container(color: AppColors.whiteColor,),
-      ),
+      routes: {
+        ForgetPasswordScreen.routeName: (_)=> ForgetPasswordScreen(),
+        OtpVerification.routeName:(_)=> OtpVerification(),
+      },
+      initialRoute: ForgetPasswordScreen.routeName,
     );
   }
 }
