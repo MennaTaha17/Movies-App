ca1a032036952b087dfe1b1a51be671f7a2679b6
shahdyassrt25 <shahdyasser25@gmail.com>
2025-11-23 20:07:58 +0200

    spalsh onboardang

---
+++ lib/screens/Spalsh Onboradang/spalsh_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _pulseAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _fadeAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+
+    _pulseAnimation = Tween<double>(begin: 0.8, end: 1.2).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+
+    Timer(const Duration(seconds: 4), () {
+      Navigator.of(context).pushReplacementNamed('/onboarding');
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF121312), Color(0xFF1E1C1F)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FadeTransition(
+                opacity: _fadeAnimation,
+                child: ScaleTransition(
+                  scale: _pulseAnimation,
+                  child: const Icon(
+                    Icons.play_arrow,
+                    size: 180,
+                    color: Colors.amber,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Column(
+                children: const [
+                  Text(
+                    'Route',
+                    style: TextStyle(
+                      color: Color(0xFFFFC107),
+                      fontSize: 28,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Supervised by Mohamed Nabil',
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
